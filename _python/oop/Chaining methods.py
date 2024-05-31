a1142b8c2fe47ca6868d48a24b2de42434b2b81a
ahmad nezam <ahmad628go@gmail.com>
2024-05-31 18:34:04 +0300

    python

--- _python/oop/Chaining methods.py
+++ _python/oop/Chaining methods.py
@@ -5,29 +5,20 @@ class User():
         self.account_balance =account_balance
     def make_deposit(self,amount):
         self.account_balance+=amount
-    
+        return self
     def make_withdrawal(self,amount):
         self.account_balance-=amount
-     
+        return self
     def display_user_balance(self):
         print('user = ',self.name,'and his balance is =',self.account_balance) 
-      
+        return self   
 
 yousef = User('yousef atta','yo@atta.com',200)
-yousef.make_deposit(100)
-yousef.make_withdrawal(100)
-yousef.display_user_balance()           
+yousef.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(100).display_user_balance()
+
 
 mahmoud = User('mahmoud','mahmoud@gmail.com',400)
-mahmoud.make_deposit(200)
-mahmoud.make_deposit(300)
-mahmoud.make_withdrawal(200)
-mahmoud.make_withdrawal(300)
-mahmoud.display_user_balance()
+mahmoud.make_deposit(200).make_deposit(300).make_withdrawal(200).make_withdrawal(300).display_user_balance()
 
 saleh = User('saleh','saleh@gmail.com',300)
-saleh.make_deposit(400)
-saleh.make_withdrawal(400)
-saleh.make_withdrawal(500)
-saleh.make_withdrawal(600)
-saleh.display_user_balance()
+saleh.make_deposit(400).make_withdrawal(400).make_withdrawal(500).make_withdrawal(600).display_user_balance()
