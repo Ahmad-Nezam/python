0e247c7e857c03b59c0ec00a9f763f71138a73cc
ahmad nezam <ahmad628go@gmail.com>
2024-06-11 15:17:16 +0300

    python

---
+++ _python/flask/flask_fundamentals/Dojo Survery/server.py
@@ -0,0 +1,29 @@
+from flask import Flask , render_template , request ,redirect
+app = Flask(__name__)
+
+@app.route("/")
+def showing():
+    return render_template("index.html")
+
+
+@app.route('/result' , methods=['POST'])
+def dojo():
+    print("got post info")
+    print(request.form)
+    print(f"name submitted :  {request.form['name']}")
+    print(f"location submitted :  {request.form['loc']}")
+    print(f"language submitted :  {request.form['lang']}")
+    print(f"comment submitted :  {request.form['com']}")
+    
+  
+
+    return render_template("info.html" ,name =request.form['name'] ,location = request.form['loc'] 
+                           , language = request.form['lang'],
+                             comment = request.form['com'])
+                          
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
--- _python/flask/flask_fundamentals/Playground/server2.py
+++ _python/flask/flask_fundamentals/Playground/server2.py
@@ -1,9 +1,11 @@
 from flask import Flask ,render_template
 app = Flask(__name__)
 
-@app.route("/Play")
-def show_box1():
-    return render_template("index.html")
+@app.route("/play")
+def show_box1(x=3,color="red"):
+    num_box = int(x)
+    box_color = color
+    return render_template("index.html",num_box = num_box,box_color=box_color)
 
 @app.route("/play/<string:x>/<string:color>")
 def show_box2(x,color):
